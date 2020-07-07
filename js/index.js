12adba8b59023285051937c7522abec9b55c119c
eduardo <eduardohmlima@hotmail.com>
2020-07-07 20:08:21 -0300

    alterações finais no texto e popup

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-let div = document.querySelector('.botao-acessar')
+let div = document.querySelector('.popup')
 
 document.querySelector('html').addEventListener('mouseleave', function(){
-     alert('Não se vá :) Vem com a gente aprender hacks para a sua saúde mental? Se inscreva no formulário da sympla, boraaa!')
+     alert('Não se vá :) Vem com a gente aprender hacks para a sua saúde mental! Se inscreva no formulário da sympla, boraaa!')
 })
 
 //Contador
