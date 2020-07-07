accf99544dbfd2d045fad22c7ec387a0a6d7f982
Fabi <fabicaneyu@gmail.com>
2020-07-06 21:10:20 -0300

    Atualizações #6

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let div = document.querySelector('.popup')
+
+
+div.addEventListener('mouseleave', function(){
+    alert('Não se vá :) Vem com a gente aprender hacks para a sua saúde mental? Se inscreva no formulário da sympla, boraaa!')
+})
+
